a4131c88da6d366150d0b2768c30117b4fe60f1c
MAX Martin <mmartin@rockhopper-03.cs.wisc.edu>
2024-02-14 11:09:34 -0600

    Initial p3 files

---
+++ p3/wsh.c
@@ -0,0 +1,14 @@
+
+
+int main(int argc, char* argv[]){
+    while(1){
+        // interactive mode
+        if(argc == 1){
+            printf("wsh> ");
+        }
+        // batch mode
+        else{
+
+        }
+    }
+}
