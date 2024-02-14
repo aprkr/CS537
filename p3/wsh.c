2820c02df18ee04ccd5ef632c7981953885809c9
Maxwell Martin <mrmartin4064@gmail.com>
2024-02-14 11:50:52 -0600

    Updated shell argument and command passing

--- p3/wsh.c
+++ p3/wsh.c
@@ -1,10 +1,24 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
     while(1){
         // interactive mode
         if(argc == 1){
+            // get and store cmd from stdin
+            char* cmd;
+            size_t cmd_len = 250;
+            cmd = (char*) malloc(cmd_len * sizeof(char));
+            if(cmd == NULL){
+                printf("Could not malloc the buffer to hold the command");
+                exit(1);
+            }
+            // prompt user for input
             printf("wsh> ");
+            getline(&cmd, &cmd_len, stdin);
+            // split up the input so the args and cmd can be passed seperatly
+            strtok();
+            execvp(cmd);
         }
         // batch mode
         else{
