813cb210bbec1f7bcd760759c2d8001d65f033b3
Maxwell Martin <mrmartin4064@gmail.com>
2024-02-16 00:41:02 -0600

    Added support for simple commands

--- p3/wsh.c
+++ p3/wsh.c
@@ -1,27 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]){
-    while(1){
-        // interactive mode
-        if(argc == 1){
-            // get and store cmd from stdin
-            char* cmd;
-            size_t cmd_len = 250;
-            cmd = (char*) malloc(cmd_len * sizeof(char));
-            if(cmd == NULL){
-                printf("Could not malloc the buffer to hold the command");
-                exit(1);
-            }
-            // prompt user for input
-            printf("wsh> ");
-            getline(&cmd, &cmd_len, stdin);
-            // split up the input so the args and cmd can be passed seperatly
-            printf("%s", cmd);
+#define MAX_INPUT_SIZE 2048
+#define MAX_ARGS 128
+
+void read_input(char *input){
+    // prompt user and get input from them
+    printf("wsh> ");
+    fgets(input, MAX_INPUT_SIZE, stdin);
+    // remove the trailing newline
+    input[strcspn(input, "\n")] = '\0';
+} 
+
+void parse_input(char* input, char *args[]){
+    // keeps track of the amount of args present
+    int argc = 0;
+    // seperates the args by spaces
+    char *token = strtok(input, " ");
+    while (token != NULL && argc < MAX_ARGS - 1) {
+        args[argc++] = token;
+        token = strtok(NULL, " ");
+    }
+    // list of args must be null-terminated to work with execvp
+    args[argc] = NULL;
+}
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+    char *args[MAX_ARGS];
+
+    while (1) {
+        // Read input from the user
+        read_input(input);
+
+        // Parse input into arguments
+        parse_input(input, args);
+
+        // Execute the command
+        pid_t pid = fork();
+
+        if(*args[0] == EOF){
+            printf("exiting\n");
+            exit(0);
         }
-        // batch mode
-        else{
-
+        if (pid < 0) {
+            perror("fork");
+        } else if (pid == 0) {
+            // Child process
+            execvp(args[0], args);
+            // If execvp returns, it means an error occurred
+            perror("execvp");
+        } else {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
         }
     }
+
+    return 0;
 }
