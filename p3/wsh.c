5a6f2d52f94ed9227c1b49e2e9d1796e882786bc
Maxwell Martin <mrmartin4064@gmail.com>
2024-02-16 01:48:57 -0600

    Fixed the basic shell exiting

--- p3/wsh.c
+++ p3/wsh.c
@@ -11,12 +11,16 @@
 void read_input(char *input){
     // prompt user and get input from them
     printf("wsh> ");
-    fgets(input, MAX_INPUT_SIZE, stdin);
+    char* res = fgets(input, MAX_INPUT_SIZE, stdin);
+    // if EOF is recieved end gracefully
+    if(res == NULL){
+        exit(0);
+    }
     // remove the trailing newline
     input[strcspn(input, "\n")] = '\0';
 } 
 
-void parse_input(char* input, char *args[]){
+int parse_input(char* input, char *args[]){
     // keeps track of the amount of args present
     int argc = 0;
     // seperates the args by spaces
@@ -27,6 +31,7 @@ void parse_input(char* input, char *args[]){
     }
     // list of args must be null-terminated to work with execvp
     args[argc] = NULL;
+    return argc;
 }
 
 int main() {
@@ -36,30 +41,25 @@ int main() {
     while (1) {
         // Read input from the user
         read_input(input);
-
         // Parse input into arguments
-        parse_input(input, args);
-
+        int argCount = parse_input(input, args);
+        // if nothing was entered just prompt again
+        if(argCount == 0){
+            continue;
+        }
         // Execute the command
         pid_t pid = fork();
-
-        if(*args[0] == EOF){
-            printf("exiting\n");
-            exit(0);
-        }
-        if (pid < 0) {
-            perror("fork");
-        } else if (pid == 0) {
-            // Child process
+        // exec the program in the child process so the shell or parent process can wait for it to finish
+        if (pid == 0) {
+            // Child process - here we will execute the command
             execvp(args[0], args);
             // If execvp returns, it means an error occurred
             perror("execvp");
         } else {
-            // Parent process
+            // Parent process - here we will wait for the child command to finish
             int status;
             waitpid(pid, &status, 0);
         }
     }
-
     return 0;
 }
