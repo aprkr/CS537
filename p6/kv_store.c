bddc955428c7932c52e59127d36a2a797a8c1334
Alex <2703parker@gmail.com>
2024-04-13 22:51:48 -0500

    Server multithreading?

--- p6/kv_store.c
+++ p6/kv_store.c
@@ -6,26 +6,55 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <pthread.h>
 
 int table_size;
 int num_threads;
 char *optarg;
+#define MAX_THREADS 128
+pthread_t threads[MAX_THREADS];
+struct ring *ring;
+void *mem;
 
 key_type *key_array;
 value_type *value_array;
+pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
 
 int put(key_type k, value_type v) {
     index_t index = hash_function(k, table_size);
+    pthread_mutex_lock(&lock2);
     value_array[index] = v;
     key_array[index] = k;
+    pthread_mutex_unlock(&lock2);
     return 0;
 
 }
 
 int get(key_type k) {
     index_t index = hash_function(k, table_size);
-    return value_array[index];
+    if (key_array[index] != 0) {
+        return value_array[index];
+    } else {
+        return 0;
+    }
 
+}
+
+void *thread_func(void *arg) {
+    // printf("thread started\n");
+    while (1) {
+        struct buffer_descriptor bd;
+        ring_get(ring, &bd);
+        if (bd.req_type == PUT) {
+            put(bd.k, bd.v);
+        } else {
+            bd.v = get(bd.k);
+        }
+        struct buffer_descriptor *result = mem + bd.res_off;
+        memcpy(result, &bd, sizeof(struct buffer_descriptor));
+        // printf("New completion, %u %u %u\n", bd.req_type, bd.k, bd.v);
+        result->ready = 1;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -51,21 +80,12 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	int fd = open("shmem_file", O_RDWR);
-    void *mem = mmap(NULL, sizeof(struct ring), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    mem = mmap(NULL, sizeof(struct ring), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     close(fd);
-    struct ring *ring = (struct ring *)mem;
-
-    while (1) {
-        struct buffer_descriptor bd;
-        ring_get(ring, &bd);
-        if (bd.req_type == PUT) {
-            put(bd.k, bd.v);
-        } else {
-            bd.v = get(bd.k);
-        }
-        // struct buffer_descriptor *idk = ring + bd.res_off;
-        struct buffer_descriptor *result = mem + bd.res_off;
-        bd.ready = 1;
-        memcpy(result, &bd, sizeof(struct buffer_descriptor));
-    }
+    ring = (struct ring *)mem;
+    for (int i = 0; i < num_threads; i++)
+		pthread_create(&threads[i], NULL, thread_func, NULL);
+    for (int i = 0; i < num_threads; i++)
+		if (pthread_join(threads[i], NULL))
+			perror("pthread_join");    
 }
